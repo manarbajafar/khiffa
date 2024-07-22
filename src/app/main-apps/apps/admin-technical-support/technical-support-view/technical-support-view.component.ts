cb5f20db79478f3ff29e1ae0ea6327fdf5755ec8
manarbajafar <manar_bajafar@hotmail.com>
2024-07-22 08:04:31 +0300

    admin ticket section

--- src/app/main-apps/apps/admin-technical-support/technical-support-view/technical-support-view.component.ts
+++ src/app/main-apps/apps/admin-technical-support/technical-support-view/technical-support-view.component.ts
@@ -7,6 +7,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TechnicalSupportViewComponent implements OnInit {
 
+  tableData = [
+    {
+      title: "مشكلة بتحويل المستحقات",
+      name: "سعد محمد",
+      date: "5 يناير 2024",
+      time:"3:00 م",
+      id: "1234",
+      status: "قيد الانتظار",
+      statusClass: "badge-on-hold",
+    },
+    {
+      title: "مشكلة بتحويل المستحقات",
+      name: "سعد محمد",
+      date: "5 يناير 2024",
+      time:"3:00 م",
+      id: "1234",
+      status: "مغلقة",
+      statusClass: "badge-closed",
+    },
+    {
+      title: "مشكلة بتحويل المستحقات",
+      name: "سعد محمد",
+      date: "5 يناير 2024",
+      time:"3:00 م",
+      id: "1234",
+      status: "مفتوحة",
+      statusClass: "badge-open",
+    },
+    {
+      title: "مشكلة بتحويل المستحقات",
+      name: "سعد محمد",
+      date: "5 يناير 2024",
+      time:"3:00 م",
+      id: "1234",
+      status: "تحت المعالجة",
+      statusClass: "badge-under-processing",
+    },
+  ];
   constructor() { }
 
   ngOnInit(): void {

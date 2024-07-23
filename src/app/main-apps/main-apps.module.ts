74c54942f7eed2067a4bf62ea9be249293bb6f1f
SaraAli00 <sarakamal00@outlook.com>
2024-07-23 13:54:21 +0300

    driver

--- src/app/main-apps/main-apps.module.ts
+++ src/app/main-apps/main-apps.module.ts
@@ -5,6 +5,13 @@ import { MainAppsRoutingModule } from './main-apps-routing.module';
 import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { ProfileComponent } from './apps/profile/profile.component';
+import { DetailedOrderComponent } from './apps/detailed-order/detailed-order.component';
+import { LogoutComponent } from './apps/logout/logout.component';
+import { OrdersComponent } from './apps/orders/orders.component';
+import { SupportComponent } from './apps/support/support.component';
+import { TicketsComponent } from './apps/tickets/tickets.component';
+import { WalletComponent } from './apps/wallet/wallet.component';
 
 
 @NgModule({
@@ -12,6 +19,13 @@ import { SidebarComponent } from './layout/sidebar/sidebar.component';
     MainLayoutComponent,
     HeaderComponent,
     SidebarComponent,
+    ProfileComponent,
+    DetailedOrderComponent,
+    LogoutComponent,
+    OrdersComponent,
+    SupportComponent,
+    TicketsComponent,
+    WalletComponent,
 
   ],
   imports: [

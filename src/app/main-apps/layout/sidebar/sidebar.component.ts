a96ae13132e46b785ea3573023f0a486a9aaad2e
SaraAli00 <sarakamal00@outlook.com>
2024-07-24 08:14:53 +0300

    تسجيل خروج

--- src/app/main-apps/layout/sidebar/sidebar.component.ts
+++ src/app/main-apps/layout/sidebar/sidebar.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit,EventEmitter, Output, HostListener, HostBinding } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';  // Ensure this import
 import { SidebarService } from './sidebar.service';
-
-
 
 @Component({
   selector: 'app-sidebar',
@@ -24,21 +24,36 @@ export class SidebarComponent implements OnInit {
     { routerLink: '/apps/wallet', label: 'المحفظة', icon: 'bi bi-wallet' },
     { routerLink: '/apps/driver-orders/orders', label: 'الطلبات', icon: 'bi bi-card-checklist' },
     { routerLink: '/apps/tickets', label: 'تذاكري', icon: 'bi bi-tools' },
-    { routerLink: '/apps/logout', label: 'تسجيل خروج', icon: 'bi bi-box-arrow-right' }
+    { routerLink: '', label: 'تسجيل خروج', icon: 'bi bi-box-arrow-right' }
   ];
 
-
-  constructor(public sidebarService: SidebarService) {}
+  constructor(
+    public sidebarService: SidebarService,
+    private router: Router,
+    private modalService: NgbModal  // Add this
+  ) {}
 
   @HostBinding('class.is-expanded')
   get isExpanded() {
     return this.sidebarService.isExpanded;
   }
 
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  openModal(content: any) {
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then(
+      (result) => {
+        if (result === 'logout') {
+          this.confirmLogout();
+        }
+      }, (reason) => {
+        console.log('Dismissed with:', reason); // Implement actual logic as needed
+      }
+    );
+  }
 
-   }
-
-
+  confirmLogout() {
+    this.modalService.dismissAll(); // Close all open modals
+    this.router.navigate(['/auth/login']); // Navigate to login page
+  }
 }

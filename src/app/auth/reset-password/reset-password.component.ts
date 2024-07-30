478e519fd9a703453a78950821166aa9ebe6df60
manarbajafar <manar_bajafar@hotmail.com>
2024-07-30 13:39:23 +0300

    modify conflicts

--- src/app/auth/reset-password/reset-password.component.ts
+++ src/app/auth/reset-password/reset-password.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
 import { AUTH } from 'src/app/constant/routes';
 
 @Component({
@@ -16,7 +17,7 @@ export class ResetPasswordComponent  {
   confirmPassword: string = '';
   submitted: boolean = false;
 
-  constructor(private http: HttpClient, private route: ActivatedRoute, private router: Router) { }
+  constructor(private http: HttpClient, private route: ActivatedRoute, private router: Router, private spinner: NgxSpinnerService) { }
 
   ngOnInit(): void {
     this.email = this.route.snapshot.queryParamMap.get('email')!;
@@ -24,15 +25,18 @@ export class ResetPasswordComponent  {
   }
 
   resetPassword() {
+    this.spinner.show();
+
     if (this.password !== this.confirmPassword) {
       alert('كلمات المرور غير متطابقة');
       return;
     }
-
     const body = { email: this.email, password: this.password, otp: this.otp };
     this.http.post(AUTH.resetPassword, body).subscribe(response => {
       this.submitted = true;
+      this.spinner.hide();
     }, error => {
+      //toaser
       alert('حدث خطأ أثناء إعادة تعيين كلمة المرور.'+ error.message);
     });
   }
--- src/app/constant/routes.ts
+++ src/app/constant/routes.ts
@@ -15,36 +15,23 @@ export const AUTH = {
 
 
 export const Order = {
-
-
-
-
   getorder: BASEURL + 'orders',
-
   assign: BASEURL + 'orders/assign',
   //filter: BASEURL + 'orders/filter',
-
-
   updateStatus: BASEURL + "orders/status?order_id=",
-
-
   details:  BASEURL + "orders/order?order_id="
 };
 
 export const transaction ={
-
   requestTransaaction: BASEURL + "transactions",
   sendTransactions: BASEURL + "sendTransactions",
   showTransactions : BASEURL + "showTransactions"
-
 }
+
 
 export const driverProfile ={
   profile: BASEURL + "profile",
   wallet : BASEURL + "wallet",
-
-
-
 }
 
 
@@ -58,11 +45,10 @@ export const ADMIN_DASHBOARD = {
   getAllOrders: BASEURL + 'orders',
 }
 
-export const admin = {
+export const ADMIN_MANAGING_DELIVERYMANS = {
   changeAccountStatus: BASEURL + 'changeAccountStatus',
-  showInfoAccountReq: BASEURL + 'showInfoAccountReq?user_id=3',
+  showInfoAccountReq: BASEURL + 'showInfoAccountReq?user_id=',
   accountRequests: BASEURL + 'accountRequests',
-
-
+  getDeliverymanList: BASEURL + 'users?page=',
 }
 
